eb98b2afad3200122f27a39c5a263dd637067b6b
João Victor de Medeiros <medeirosjoaoadv@gmail.com>
2021-07-24 18:49:27 -0300

    Convertendo para o hooks

--- basico-posts/src/templates/home/index.jsx
+++ basico-posts/src/templates/home/index.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 import "./styles.css";
 import { Posts } from "../../components/Posts/index";
@@ -9,7 +9,73 @@ import { Button } from "../../components/Button/index";
 
 import { TextInput } from "../../components/TextInput/index"
 
-class Home extends Component {
+export const Home = () => {
+
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage, setPostsPerPage] = useState(5);
+  const [searchValue, setSearchValue] = useState('');
+
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+
+  const filteredPosts = !!searchValue ? allPosts.filter((post) => {
+    return post.title.toLowerCase().includes(searchValue.toLowerCase());
+  })
+: posts;
+
+const handleLoadPosts = useCallback (async (page, postsPerPage) => {
+
+  const postsAndPhotos = await loadPosts();
+
+  setPosts(postsAndPhotos.slice(page, postsPerPage))
+  setAllPosts(postsAndPhotos)
+}, []);
+
+useEffect(() => { 
+  handleLoadPosts(0, postsPerPage);
+}, [handleLoadPosts, postsPerPage]);
+
+const loadMorePosts = () => {
+  const nextPage = page + postsPerPage;
+  const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+  posts.push(...nextPosts);
+
+  setPosts(posts);
+  setPage(nextPage);
+};
+
+const handleChange = (e) => {
+  const { value } = e.target;
+  setSearchValue(value)
+};
+
+  return (
+    <section className="container">
+      <div className="search-container">
+      {!!searchValue && (<h1>Search value: {searchValue}</h1>)}
+      {/* a expressão !! converte o valor para booleano. O intuito é utilizar a avaliação de curto circuito do JS. Nesse caso se for falso faça o que está entre parenteses*/}
+      {/* Acima ele está dizendo: se houver busca, exiba o search value.*/}
+      <TextInput searchValue={searchValue} handleChange={handleChange}/>
+      </div>
+
+      {filteredPosts.length > 0 ? <Posts posts={filteredPosts} /> : 'Não existem posts com esse valor de busca.'}
+
+      <div className="button-container">
+        {!searchValue && (
+          <Button
+            text="Load Posts"
+            onClick={loadMorePosts}
+            disabled={noMorePosts}
+          />
+        )}
+        {/* Acima ele está dizendo: se não houver busca, quero exibir o botão.*/}
+      </div>
+    </section>
+  );
+}
+
+/* class Home2 extends Component {
   state = {
     posts: [],
     allPosts: [],
@@ -52,13 +118,13 @@ class Home extends Component {
           return post.title.toLowerCase().includes(searchValue.toLowerCase());
         })
       : posts;
-         {/* Acima está a lógica para a busca do posts.*/}
+         {/* Acima está a lógica para a busca do posts.
     return (
       <section className="container">
         <div className="search-container">
         {!!searchValue && (<h1>Search value: {searchValue}</h1>)}
-        {/* a expressão !! converte o valor para booleano. O intuito é utilizar a avaliação de curto circuito do JS. Nesse caso se for falso faça o que está entre parenteses*/}
-        {/* Acima ele está dizendo: se houver busca, exiba o search value.*/}
+        {/* a expressão !! converte o valor para booleano. O intuito é utilizar a avaliação de curto circuito do JS. Nesse caso se for falso faça o que está entre parenteses*/
+        /* Acima ele está dizendo: se houver busca, exiba o search value.
         <TextInput searchValue={searchValue} handleChange={this.handleChange}/>
         </div>
 
@@ -72,10 +138,12 @@ class Home extends Component {
               disabled={noMorePosts}
             />
           )}
-          {/* Acima ele está dizendo: se não houver busca, quero exibir o botão.*/}
+          {/* Acima ele está dizendo: se não houver busca, quero exibir o botão.
         </div>
       </section>
     );
   }
-}
+} */
+
+
 export default Home;
